58eba6b694dc52241a9cd6f81ede6f1f9a1d5ffb
52110919 <52110919@qq.com>
2019-08-21 09:50:37 +0800

    update

--- config-client/src/main/java/cn/lixinblog/ConfigClientApplication.java
+++ config-client/src/main/java/cn/lixinblog/ConfigClientApplication.java
@@ -3,15 +3,13 @@ package cn.lixinblog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@EnableConfigServer
 @EnableDiscoveryClient
-public class ConfigApplication {
+public class ConfigClientApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConfigApplication.class);
+        SpringApplication.run(ConfigClientApplication.class);
     }
 
 }
